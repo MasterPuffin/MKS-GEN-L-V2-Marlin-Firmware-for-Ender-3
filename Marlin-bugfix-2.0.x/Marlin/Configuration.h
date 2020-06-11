9b451e222e0b1898dae0eb3d29522d5e53bbbe84
Johannes <johannes.bluhm@outlook.com>
2020-06-11 18:23:15 +0200

    changed probe offset

--- Marlin-bugfix-2.0.x/Marlin/Configuration.h
+++ Marlin-bugfix-2.0.x/Marlin/Configuration.h
@@ -961,11 +961,11 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -42, -5, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -41, -10, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
-#define MIN_PROBE_EDGE 10
+#define MIN_PROBE_EDGE 30
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
